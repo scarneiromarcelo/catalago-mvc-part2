ee3a81da51effc1d7f64437ac1f243aecd4b0e6a
scarneiromarcelo <s.carneiro.marcelo@gmail.com>
2022-06-10 22:27:12 -0300

    improvement commit

--- src/controllers/MotoController.js
+++ src/controllers/MotoController.js
@@ -2,13 +2,78 @@ const Moto = require("../models/Moto");
 
 const getAll = async (req, res) => {
   try {
-    const motos = await Moto.findAll(); //findAll é uma função do Sequelize
-    res.render("index", { motos, moto: undefined });
+    const motos = await Moto.findAll();
+    res.render("index", { motos, motoPut: null, pokemonDel: null });
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
 };
 
+const signup = (req, res) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+const create = async (req, res) => {
+  try {
+    const moto = req.body;
+
+    if (!moto) {
+      return res.redirect("/signup");
+    }
+
+    await Moto.create(moto);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const method = req.params.method;
+    const motos = await Moto.findAll();
+    const moto = await Moto.findByPk(req.params.id)
+
+    if (method == 'put'){
+      res.render("index", {
+        motos,
+        motoPut: moto,
+        motoDel: null, 
+      })
+    } else {
+      res.render("index", {
+        motos,
+        motoPut: null,
+        motoDel: moto, 
+    })
+  }
+
+
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+    
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const moto = req.body;
+    await Moto.update(moto, {where: {id: req.params.id}})
+    res.redirect("/")
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+    
+  }
+}
+
+
 module.exports = {
   getAll,
+  signup,
+  create,
+  getById,
+  update
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,9 @@
 const routes = require("express").Router();
 const MotoController = require("../controllers/MotoController");
 routes.get("/", MotoController.getAll);
+routes.get("/signup", MotoController.signup);
+routes.post("/create", MotoController.create);
+routes.get("/getById/:id/:method", MotoController.getById);
+routes.post("/update/:id", MotoController.update);
 
 module.exports = routes;
